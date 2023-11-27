58bee414038ed182f8effdfab5dd75601fdaa281
Gideon Isa <innocentgideon10@gmail.com>
2023-11-28 00:44:53 +0100

    Region controller endpoint

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -36,7 +36,6 @@ namespace NZWalks.API.Controllers
                 });
             }
 
-
             return Ok(regionsDto);
         }
 
@@ -53,7 +52,105 @@ namespace NZWalks.API.Controllers
                 return NotFound();
             }
 
-            return Ok(region);
+            var regionDto = new RegionDto
+            {
+                Id  = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+
+            return Ok(regionDto);
         }
+
+        [Route("")]
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map or Convert DTO to Domain Model
+            Region newRegion = new Region()
+            {
+               
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // SQL Server automatically generate a guid; No need to generate a new Guid value for newRegion model
+
+            // Use Domain Model to create Region
+            dbContext.Regions.Add(newRegion);
+            dbContext.SaveChanges(); // as it saves, guid is generated
+
+            var regionDto = new RegionDto()
+            {
+                Id = newRegion.Id,
+                Code = newRegion.Code,
+                Name = newRegion.Name,
+                RegionImageUrl = newRegion.RegionImageUrl,
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = newRegion.Id }, regionDto); 
+
+        }
+
+        [Route("{id:Guid}")]
+        [HttpPut]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto update)
+        {
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Map Dto to Domain model
+            regionDomainModel.Name = update.Name;
+            regionDomainModel.Code = update.Code;
+            regionDomainModel.RegionImageUrl = update.RegionImageUrl;
+
+            // saving to database
+            dbContext.SaveChanges();
+
+            //Convert Domain Model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+
+        [Route("{id:Guid}")]
+        [HttpDelete]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Delete region
+            dbContext.Regions.Remove(regionDomainModel);
+            dbContext.SaveChanges(); // This makes the changes effective in the database
+
+            // Map
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+
     }
 }
